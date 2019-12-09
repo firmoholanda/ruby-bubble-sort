73c268fb3826f11b50573ddd1ecfd904fcdb51e6
ricardovaltierra <ricardo_valtierra@outlook.com>
2019-12-09 11:17:30 -0600

    buble sorting algorithm with for cicle

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,13 +1,19 @@
 def bubble_sort(array)
   n = array.lenght
 
-  do
+  loop do
     newN = 0
-    for i = 1 
-  end
-
+  
+    for i = 1 in n-1 do 
+      if array[i-1] > a[i]
+        array[i], array[i + 1] = array[i + 1], array[i] 
+        newN = i
+      end
+    end
+    n = newN
+  break if n <= 1
 end
 
 array = [1,3,5,2,4]
 
-bubble_sort [array]
+bubble_sort(array)
