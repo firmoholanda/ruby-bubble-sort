e2f00434671acfc6de6f05d444413fe97f5a5d22
firmo holanda <firmo@viize.com>
2019-12-09 14:49:20 -0300

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,16 @@
 def bubble_sort(array)
   n = array.length
-  begin
+  loop do
     newN = 0
-     (0..n-1).each do |i|
-      #puts i.to_s + " interation"
+     for i in (1..n-1)
       if array[i-1] > array[i]
         array[i-1], array[i] = array[i], array[i-1] 
         newN = i
       end
     end
     n = newN
-  end while n <= 1
+    break if n <= 1
+  end
   return array
 end
 
