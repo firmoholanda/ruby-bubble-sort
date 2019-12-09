220a9b2e050163472f8662cef7b34019200a9704
ricardovaltierra <ricardo_valtierra@outlook.com>
2019-12-09 13:17:11 -0600

    stickler validation

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,18 +3,18 @@
 def bubble_sort_by(array)
   n = array.length
   loop do
-    newN = 0
-    (1..n -1 ).each do |i|
-      result = yield(array[i-1], array[i])      
+    new_n = 0
+    (1..n - 1).each do |i|
+      result = yield(array[i-1], array[i])
       if result > 0
-        array[i-1], array[i] = array[i], array[i-1] 
-        newN = i
+        array[i-1], array[i] = array[i], array[i-1]
+        new_n = i
       end
     end
-    n = newN
+    n = new_n
     break if n <= 1
   end
-  return array
+  array
 end
 
 def bubble_sort(array)
