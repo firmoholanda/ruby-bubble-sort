5d7c0b88506348916f4b33690c1382780e989b5c
firmo holanda <firmo@viize.com>
2019-12-09 16:10:57 -0300

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 def bubble_sort_by(array)
   n = array.length
   loop do
     newN = 0
-    for i in (1..n-1)
+    (1..n-1).each do |i|
       result = yield(array[i-1],array[i])      
       if result > 0
         array[i-1], array[i] = array[i], array[i-1] 
